e577a3803aba363cf13d315ac54fdfdbb449a0ed
BillCrowther <32566845+BillCrowther@users.noreply.github.com>
2022-04-25 22:46:43 +0100

    holy smoke

---
+++ Scripts/AlignParticleWithWind.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlignParticleWithWind : MonoBehaviour
+{
+    ParticleSystem.MainModule ps;
+    public GlobalWind globalWind;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        ps = GetComponent<ParticleSystem>().main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = Quaternion.FromToRotation(new Vector3(0, 0, 1), globalWind.earthWindVector);
+        ps.startSpeed = globalWind.earthWindVector.magnitude/5;
+    }
+}
